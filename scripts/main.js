3465f09cc0b31bc657031249f7ed54ee58a3e07b
Jules Dajon-Lamare <jules.dajonlamare@gmail.com>
2019-03-19 16:49:44 +0100

    test

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+function bob()
+{
+if (window.confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please')) 
+	{ 
+  	window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+  	}
+}
+
+setTimeout(bob, 10000);
